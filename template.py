b3f2e9693dc51a7a4d92cae8f3469510c1ab67aa
41039138 <113663778+GUJIAFENG@users.noreply.github.com>
2024-10-14 13:18:47 +0800

    Add files via upload

---
+++ template.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="熱流力實驗", layout="wide")
+
+st.title("熱流力實驗第七組")
+
+st.write("歡迎來到熱流力實驗報告。請從左側選單選擇要查看的實驗項目。")
+
+# 不需要在這裡定義實驗項目列表，因為Streamlit會自動生成側邊欄
